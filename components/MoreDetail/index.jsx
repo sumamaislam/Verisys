dc0e8b442261854626d07f8184973a617e138c9e
SyedFakharAbbass <mailtofakharabbas@gmail.com>
2023-02-07 19:28:56 +0500

    okay

--- components/MoreDetail/index.jsx
+++ components/MoreDetail/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-function MoreDetails() {
+function MoreDetail() {
   return (
     <div>
-      <div className='containers'>
-        <div className='adjust_padding'>
-        <h1 className='text-[35px] font-bold text-[#0F7CC0] text-center '>Verisys</h1>
+      <div className='adjust_padding'>
+        <div className="bg-[url('/images/verisysprotect.png')] bg-cover bg-left md:bg-center pt-[45px] mt-20 ">
+          <div className='containers'>
+            <h1 className='text-[50px] text-[#FFFFFF] font-bold mt-[35px]'><span className='text-[#3C9E48]'>VERISYS</span> Protect Your Product</h1>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default MoreDetails
+export default MoreDetail
